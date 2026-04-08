7b119b77f93f745953eb7935209ab74c3da08a22
Sabrina Raiane L. Barbosa <sabrinaraiane2003@gmail.com>
2026-04-08 17:01:15 -0300

    feat(repository): criar AdministradorRepository estendendo JpaRepository

---
+++ src/main/java/com/qjrun/qjrun/repository/AdministradorRepository.java
@@ -0,0 +1,7 @@
+package com.qjrun.qjrun.repository;
+
+import com.qjrun.qjrun.entity.Administrador;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdministradorRepository extends JpaRepository<Administrador,Long> {
+}
